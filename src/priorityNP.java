90f94736f467e1971a97baf7d2b125244e55d02e
louis <louisse.evale.iics@ust.edu.ph>
2020-12-06 02:39:08 +0800

    DONE NA TALAGA pinaka final

---
+++ src/priorityNP.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+
+public class priorityNP {
+
+    static void sortProcesses(String pid[], int n, int at[], int bt[], int prt[]) {
+
+        int temp;
+        String stemp;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (at[j] > at[j + 1]) {
+                    // swapping arrival time
+                    temp = at[j];
+                    at[j] = at[j + 1];
+                    at[j + 1] = temp;
+
+                    // swapping burst time
+                    temp = bt[j];
+                    bt[j] = bt[j + 1];
+                    bt[j + 1] = temp;
+
+                    // swapping priority
+                    temp = prt[j];
+                    prt[j] = prt[j + 1];
+                    prt[j + 1] = temp;
+
+                    // swapping process identity
+                    stemp = pid[j];
+                    pid[j] = pid[j + 1];
+                    pid[j + 1] = stemp;
+                }
+
+                if (at[j] == at[j + 1]) {
+                    if (prt[j] > prt[j + 1]) {
+                        // swapping arrival time
+                        temp = at[j];
+                        at[j] = at[j + 1];
+                        at[j + 1] = temp;
+
+                        // swapping burst time
+                        temp = bt[j];
+                        bt[j] = bt[j + 1];
+                        bt[j + 1] = temp;
+
+                        // swapping priority
+                        temp = prt[j];
+                        prt[j] = prt[j + 1];
+                        prt[j + 1] = temp;
+
+                        // swapping process identity
+                        stemp = pid[j];
+                        pid[j] = pid[j + 1];
+                        pid[j + 1] = stemp;
+                    }
+                }
+            }
+        }
+    }
+
+    static void algorithm(String pid[], int n, int at[], int bt[], int prt[]) {
+
+        int ct[] = new int[n], tat[] = new int[n], wt[] = new int[n], total_wt = 0, total_tat = 0;
+
+        sortProcesses(pid, n, at, bt, prt);
+
+        ct[0] = at[0] + bt[0];
+        tat[0] = ct[0] - at[0];
+        wt[0] = tat[0] - bt[0];
+
+        for (int i = 1; i < n; i++) {
+            ct[i] = bt[i] + ct[i - 1];
+            tat[i] = ct[i] - at[i];
+            wt[i] = tat[i] - bt[i];
+        }
+
+        System.out.println("Processes | " + "Arrival Time | " + "Burst Time | " + "Completion Time | " + "Turn Around Time | " + "Waiting Time");
+
+        for (int i = 0; i < n; i++) {
+            total_wt = total_wt + wt[i];
+            total_tat = total_tat + tat[i];
+
+            System.out.println("P" + (i+1) + "\t\t" + at[i] + "\t\t" + bt[i] + "\t\t" + ct[i] + "\t\t" + tat[i] + "\t\t" + wt[i] );
+        }   
+        
+        System.out.println("Average waiting time: " + (float)total_wt / (float)n);
+        System.out.println("Average turn around time: " + (float)total_tat / (float)n);
+
+    }
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of processes [2-9]: ");
+        int userIn = sc.nextInt();
+
+        String pid[] = new String[userIn];
+        int at[] = new int[userIn], bt[] = new int[userIn], prt[] = new int[userIn];
+        int n = pid.length;
+
+        System.out.println("Arrival Time");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter arrival time for P" + (i+1) + ": ");
+            at[i] = sc.nextInt();
+        }
+
+        System.out.println("Burst Time");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter burst time for P" + (i+1) + ": ");
+            bt[i] = sc.nextInt();
+        }
+
+        System.out.println("Priority");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter priority for P" + (i+1) + ": ");
+            prt[i] = sc.nextInt();
+        }     
+
+        algorithm(pid, n, at, bt, prt);    
+
+        sc.close();
+    }
+}
--- src/roundrobin.java
+++ src/roundrobin.java
@@ -2,38 +2,8 @@ import java.util.Scanner;
 
 public class roundRobin {
 
-    public static void main(String[] args) throws Exception {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter number of processes [2-9]: ");
-        int userIn = sc.nextInt();
-
-        int p[] = new int[userIn], at[] = new int[userIn], bt[] = new int[userIn];
-        int n = p.length;
-
-        System.out.println("Arrival Time");
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter arrival time for P" + (i+1) + ": ");
-            at[i] = sc.nextInt();
-        }
-
-        System.out.println("Burst Time");
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter burst time for P" + (i+1) + ": ");
-            bt[i] = sc.nextInt();
-        }
-
-        System.out.print("Enter Time Quantum: ");
-        int quantum = sc.nextInt();
-
-        avgTime(p, n, bt, quantum, at);
-
-        sc.close();
-
-    }   
-
-    static void waitingTime(int processes[], int n, int bt[], int wt[], int quantum, int ct[], int at[]) {
+    static void waitingTime(String pid[], int n, int at[], int bt[], int wt[], int qt, int ct[]) {
+        
         int rem_bt[] = new int[n];
 
         for (int i = 0; i < n; i++)
@@ -48,9 +18,9 @@ public class roundRobin {
                 if (rem_bt[i] > 0) {
                     done = false;
 
-                    if (rem_bt[i] > quantum & at[i] <= arrival) {
-                        t += quantum;
-                        rem_bt[i] -= quantum;
+                    if (rem_bt[i] > qt & at[i] <= arrival) {
+                        t += qt;
+                        rem_bt[i] -= qt;
                         arrival++;
                     } else {
                         t = t + rem_bt[i];
@@ -65,30 +35,63 @@ public class roundRobin {
         }        
     }
 
-    static void turnaroundTime(int processes[], int n, int bt[], int wt[], int tat[], int ct[], int at[]) {
+    static void turnaroundTime(String pid[], int n, int at[], int bt[], int tat[], int wt[], int ct[]) {
         for (int i = 0; i < n; i++) {
             tat[i] = ct[i] - at[i];
             wt[i] = tat[i] - bt[i];
         }
     }
     
-    static void avgTime(int processes[], int n, int bt[], int quantum, int at[]) {
-        int wt[] = new int[n], tat[] = new int[n];
-        int total_wt = 0, total_tat = 0;
-        int ct[] = new int[n];
+    static void avgTime(String pid[], int n, int at[], int bt[], int qt) {
 
-        waitingTime(processes, n, bt, wt, quantum, ct, at);
-        turnaroundTime(processes, n, bt, wt, tat, ct, at);
+        int wt[] = new int[n], tat[] = new int[n], ct[] = new int[n], total_wt = 0, total_tat = 0;
+
+        waitingTime(pid, n, at, bt, wt, qt, ct);
+        turnaroundTime(pid, n, at, bt, tat, wt, ct);
 
         System.out.println("Processes | " + "Arrival Time | " + "Burst Time | " + "Completion Time | " + "Turn Around Time | " + "Waiting Time");
 
         for (int i = 0; i < n; i++) {
             total_wt = total_wt + wt[i];
             total_tat = total_tat + tat[i];
+
             System.out.println("P" + (i+1) + "\t\t" + at[i] + "\t\t" + bt[i] + "\t\t" + ct[i] + "\t\t" + tat[i] + "\t\t" + wt[i] );
         }
 
         System.out.println("Average waiting time: " + (float)total_wt / (float)n);
         System.out.println("Average turn around time: " + (float)total_tat / (float)n);
+
     }
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of processes [2-9]: ");
+        int userIn = sc.nextInt();
+
+        String pid[] = new String[userIn];
+        int at[] = new int[userIn], bt[] = new int[userIn];
+        int n = pid.length;
+
+        System.out.println("Arrival Time");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter arrival time for P" + (i+1) + ": ");
+            at[i] = sc.nextInt();
+        }
+
+        System.out.println("Burst Time");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter burst time for P" + (i+1) + ": ");
+            bt[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter Time Quantum: ");
+        int qt = sc.nextInt();
+
+        avgTime(pid, n, at, bt, qt);
+        
+        sc.close();
+
+    }   
 }
